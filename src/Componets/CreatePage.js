9fdebc152ecd95e9b81cd063a63565e0633d0592
Nicole Martin <n.martinpdx@gmail.com>
2021-02-24 17:20:09 -0800

    File tree made and miro uploaded to README

---
+++ src/Componets/CreatePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class CreatePage extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>
+        Create Page
+      </div>
+    )
+  }
+}
+ 
---
+++ src/api-utils.js
@@ -0,0 +1,1 @@
+// Functions will go here
